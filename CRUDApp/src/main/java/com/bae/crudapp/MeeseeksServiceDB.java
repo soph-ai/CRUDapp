4e5afe005adb6b65fd10745d32ee1977b5f68109
soph-havell <SHavell@academytrainee.com>
2021-03-07 13:53:52 +0000

    override methods from the service interface

--- CRUDApp/src/main/java/com/bae/crudapp/MeeseeksServiceDB.java
+++ CRUDApp/src/main/java/com/bae/crudapp/MeeseeksServiceDB.java
@@ -1,8 +1,9 @@
 package com.bae.crudapp;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
-import com.bae.FirstSpring.slothChallenge.SlothRepo;
+import org.springframework.stereotype.Service;
 
 @Service 
 public class MeeseeksServiceDB implements MeeseeksServiceInterface {
@@ -14,6 +15,42 @@ public class MeeseeksServiceDB implements MeeseeksServiceInterface {
 		this.repo = repo;
 	}
 	
-	
+	//override the methods from the MeeseeksServiceInterface 
+	@Override
+	public MrMeeseeks createMeeseeks(MrMeeseeks meeseeks) {
+		return this.repo.save(meeseeks);
+	}
 
+	@Override
+	public List<MrMeeseeks> getMeeseeks() {
+		return this.repo.findAll();
+	}
+
+	@Override
+	public MrMeeseeks getMeeseeksById(Long id) {
+		Optional<MrMeeseeks> optMeeseeks = this.repo.findById(id);
+		return optMeeseeks.orElse(null);
+	}
+	  
+	@Override
+	public MrMeeseeks updateMeeseeks(Long id, MrMeeseeks newMeeseeks) {
+		MrMeeseeks existing = this.getMeeseeksById(id);
+
+		existing.setDateActivated(newMeeseeks.getDateActivated());
+		existing.setName(newMeeseeks.getName());
+		existing.setPurpose(newMeeseeks.getPurpose());
+
+		return this.repo.save(existing);
+	}
+
+	@Override
+	public boolean expireMeeseeks(Long id) {
+		this.repo.deleteById(id);
+		return !this.repo.existsById(id);
+	}
+
+	@Override
+	public MrMeeseeks getMeeseeksByName(String name) {
+		return this.repo.findByName(name);
+	}
 }
