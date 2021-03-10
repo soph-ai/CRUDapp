5b4071dbb758cacad87913bc3ed32283bf1575ab
soph-havell <SHavell@academytrainee.com>
2021-03-10 19:38:45 +0000

    DB Mockito tests

--- CRUDApp/src/main/java/com/bae/CRUDApp/domain/Meeseeks.java
+++ CRUDApp/src/main/java/com/bae/CRUDApp/domain/Meeseeks.java
@@ -1,7 +1,4 @@
-package com.bae.CRUDApp;
-
-
-import java.time.LocalDate;
+package com.bae.CRUDApp.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,11 +6,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class MrMeeseeks {
+public class Meeseeks {
 	
 	private String name;
 	private String purpose;
-	private LocalDate dateActivated;
+	private String dateActivated;
 	
 	//set id as primary key (auto-incremented)
 	@Id 
@@ -21,12 +18,12 @@ public class MrMeeseeks {
 	private Long id;
 	
 	//default constructor 
-	public MrMeeseeks() {
+	public Meeseeks() {
 		super();
 	}
 	
 	//constructor 
-	public MrMeeseeks(String name, String purpose, LocalDate dateActivated) {
+	public Meeseeks(String name, String purpose, String dateActivated) {
 		super();
 		this.name = name; 
 		this.purpose = purpose;
@@ -34,7 +31,7 @@ public class MrMeeseeks {
 	}
 	
 	// constructor(2) for testing 
-	public MrMeeseeks(Long id, String name, String purpose, LocalDate dateActivated) {
+	public Meeseeks(Long id, String name, String purpose, String dateActivated) {
 		super();
 		this.name = name;
 		this.purpose = purpose;
@@ -59,11 +56,11 @@ public class MrMeeseeks {
 		this.purpose = purpose;
 	}
 
-	public LocalDate getDateActivated() {
+	public String getDateActivated() {
 		return dateActivated;
 	}
 
-	public void setDateActivated(LocalDate dateActivated) {
+	public void setDateActivated(String dateActivated) {
 		this.dateActivated = dateActivated;
 	}
 
@@ -74,4 +71,53 @@ public class MrMeeseeks {
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+	@Override
+	public String toString() {
+		return "MrMeeseeks [name=" + name + ", purpose=" + purpose + ", dateActivated=" + dateActivated + ", id=" + id
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dateActivated == null) ? 0 : dateActivated.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((purpose == null) ? 0 : purpose.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Meeseeks other = (Meeseeks) obj;
+		if (dateActivated == null) {
+			if (other.dateActivated != null)
+				return false;
+		} else if (!dateActivated.equals(other.dateActivated))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (purpose == null) {
+			if (other.purpose != null)
+				return false;
+		} else if (!purpose.equals(other.purpose))
+			return false;
+		return true;
+	}	
 }
